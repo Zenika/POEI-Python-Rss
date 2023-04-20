668b4807d86ed0a9d5bd6191ab79385ee967452b
Bullfrog666 <syczjason@gmail.com>
2023-04-20 15:44:37 +0200

    [+] add swagger test api

---
+++ api/version_1/server.py
@@ -0,0 +1,13 @@
+
+from flask import (
+    Flask,
+    render_template
+)
+
+# Creation de l'instance de l'application
+app = Flask(__name__, template_folder="../templates")
+
+# Création d'une URL pour "/"
+@app.route('/')
+def home():
+    return render_template('home.html')
